a9b0824069752050795bb7c2c0cea0b8fca86149
ordinary-student <52038952+ordinary-student@users.noreply.github.com>
2019-06-20 22:14:53 +0800

    test
    this is a test

---
+++ HelloJava/src/test/HelloJava.java
@@ -0,0 +1,38 @@
+package test;
+
+/**
+ * This is a test.
+ * 
+ * @author ordinary-student
+ * 
+ *         time:2019-06-20
+ */
+public class HelloJava
+{
+	/**
+	 * constructor
+	 */
+	public HelloJava()
+	{
+	}
+
+	/**
+	 * Main Method
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// say hello
+		new HelloJava().sayHello();
+	}
+
+	/**
+	 * Function:say hello java
+	 */
+	public void sayHello()
+	{
+		System.out.println("HelloJava!");
+	}
+
+}
